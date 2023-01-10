b908810f1280051d20dcbaad53fdfaf8ca475d44
Wenhao LUO <Wenhao.Luo@telecomnancy.eu>
2023-01-10 16:09:41 -0500

    add missing functions

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# cars
+# Cars
+
 This is for the visualization of my grandpa's conception.
+
+## Description of the concept
+
+This idea of this program is to simulate the function of a parking slot of multiple stages. To do the work, the parking slot is simulated by a program python, which track the used and available parking slots in an `array`. Lator on I am planning to show it in a GUI by using the module of `matplotlib`, specially the function `matshow`.
+
+The development of this program is divided into several stages:
+
+1. create a `Storage` class that save the parking slots, including the array; in which class there will be all the basic operation, e.g. addint a new car, removing an existing car;
+2. implement the algorithms of graph that calculate the movement of cars, these movements will be calculated then saved as individual variables, so that they can be used seperately;
+3. create a class that do the visualization, including animation, maybe also a textualized version so that it can be easily understood.
+
+## Basic movements
+
+The basic movements are the following:
+
+- `add`, `remove`: to add or to remove a car to or from the parking slots;
+- `pop`, `popleft`: to move a car into the platform that can rise or descend a car;
+- `up`, `upleft`: to lift a car on the platform on the right side/left side;
+- `down`, `downleft`: to descend a car on the platform on the right side/left side.
+
+## Data representation
+
+The data is saved in an array with the help of class `numpy.array`. The data saves the information of all parking slots in 2D, the following code is a example:
+
+```python
+# example of a parking slot array, six cars in total
+# three cars on the ground, two on the second floor, one on the third
+data = np.array(
+    [
+        [0, 0, 0, 1, 0],
+        [0, 2, 3, 0, 0],
+        [8, 7, 4, 0, 0]
+    ]
+)
+```
--- storage.py
+++ storage.py
@@ -26,12 +26,21 @@ class Storage:
         self.width = width
         self.height = height
 
+        # the following integers represent the current level of the moving platform,
+        # numbering from 0 to the (height - 1)
+        self.left_pad_level = 0
+        self.right_pad_level = 0
+
     def __str__(self) -> str:
         return str(self.data)
 
     def add(self, col_num, value) -> bool:
         if col_num not in range(1, self.width):
             return False
+        if col_num == 0 and self.left_pad_level != 0:
+            return False
+        if col_num == self.width - 1 and self.right_pad_level != 0:
+            return 0
         if self.data[0, col_num] != 0:
             return False
 
@@ -49,11 +58,14 @@ class Storage:
         if level not in range(self.height):
             return False
 
+        if level != self.right_pad_level:
+            return False
+
         level = (self.height - 1) - level
         if self.data[level, -1] != 0:
             return False
 
-        for i in reversed(range(1, self.width - 1)):
+        for i in reversed(range(1, self.width)):
             self.data[level][i] = self.data[level][i - 1]
         self.data[level][0] = 0
 
@@ -61,22 +73,108 @@ class Storage:
         if level not in range(self.height):
             return False
 
+        if level != self.left_pad_level:
+            return False
+
         level = (self.height - 1) - level
         if self.data[level, 0] != 0:
             return False
 
-        for i in range(0, self.width - 2):
+        for i in range(0, self.width - 1):
             self.data[level][i] = self.data[level][i + 1]
-        self.data[level]
+        self.data[level][self.width - 1] = 0
+
+    def up(self) -> bool:
+        if self.right_pad_level >= self.height - 1:
+            return False
+
+        level = self.height - self.right_pad_level - 1
+
+        if self.data[level, self.width - 1] != 0:
+            self.data[level - 1 , -1] = self.data[level, -1]
+            self.data[level, -1] = 0
+
+        self.right_pad_level += 1
+
+        return True
+    
+    def upleft(self) -> bool:
+        if self.left_pad_level >= self.height - 1:
+            return False
+
+        level = self.height - self.left_pad_level - 1
+
+        if self.data[level, 0] != 0:
+            self.data[level - 1 , 0] = self.data[level, 0]
+            self.data[level, 0] = 0
+
+        self.left_pad_level += 1
+
+        return True
+
+    def down(self) -> bool:
+        if self.right_pad_level <= 0:
+            return False
+
+        level = self.height - self.right_pad_level - 1
+
+        if self.data[level, -1] != 0:
+            self.data[level + 1 , -1] = self.data[level, -1]
+            self.data[level, -1] = 0
+
+        self.right_pad_level -= 1
+
+        return True
+
+    def downleft(self) -> bool:
+        if self.left_pad_level <= 0:
+            return False
+
+        level = self.height - self.left_pad_level - 1
+
+        if self.data[level, 0] != 0:
+            self.data[level + 1 , 0] = self.data[level, 0]
+            self.data[level, 0] = 0
+
+        self.left_pad_level -= 1
+
+        return True
+
 
 if __name__ == '__main__':
     storage = Storage(7, 5)
     print(storage)
+    print("")
 
     storage.add(3, value=1)
     storage.add(4, value=2)
+    storage.add(5, value=3)
     print(storage)
+    print("")
 
     storage.pop(0)
     print(storage)
-        
+    print("")
+
+    storage.up()
+    print(storage)
+    print("")
+
+    storage.down()
+    print(storage)
+    print("")
+
+    storage.popleft(0)
+    storage.popleft(0)
+    storage.popleft(0)
+    storage.popleft(0)
+    print(storage)
+    print("")
+
+    storage.upleft()
+    print(storage)
+    print("")
+
+    storage.downleft()
+    print(storage)
+    print("")
